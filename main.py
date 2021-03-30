61e051402a568f9a0161e839171df5f31ea09a87
Shrey Sudhir <shrey.somaiya@gmail.com>
2021-03-30 20:02:36 +1100

    Add prune_unverified
    Add command to remove everyone with "unverified" role

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import time
 import discord
-import csv
 from discord.ext import commands
 from discord.utils import get, find
 
@@ -14,7 +13,7 @@ intents.members = True
 client = commands.Bot(command_prefix="!", intents=intents)
 ROLE_CHANNEL_ID = 0
 ROLELOG_CHANNEL_ID = 0
-
+VERFIED_ROLE="unverified"
 
 @client.event
 async def on_ready():
@@ -35,11 +34,12 @@ async def on_message(message):
         await message.delete()
 
 
-# Count the number of members with a certain role 
 @client.command()
 @commands.has_permissions(administrator=True)
-async def countmembers(ctx, *role_names):
-    
+async def countmembers(ctx, *role_names):   
+    """
+    Count the number of members with a certain role 
+    """
     roletosearch = " ".join(role_names)
 
     role = find(lambda r: roletosearch.lower() == r.name.lower(), ctx.guild.roles)
@@ -49,48 +49,75 @@ async def countmembers(ctx, *role_names):
     except:
         await ctx.send(f"`{roletosearch}` was not found. Please make sure the spelling and capitalisation is correct")
 
+@client.command()
+@commands.has_permissions(administrator=True)
+async def remove_unverified(ctx):
+    """
+        Admin command to remove all unverified members from the discord.
+        Prints out a count of how many unverified members removed. 
+        Also gives reason for kick. 
+    """
+    i = 0
+    for member in ctx.message.server.members:
+        if get(member.roles, name=UNVERFIED_ROLE):
+            i += 1
+            await bot.kick(member, "You have been removed from the CSESoc Server - as you have not verified via the instructions in #welcome")
+    
+    await ctx.send(f"Removed {i} unverified members")
+    print(f"Removed {i} unverified members")
 
-# Change the command prefix during runtime 
 @client.command()
 @commands.has_permissions(administrator=True)
 async def changeprefix(ctx, newprefix):
+    """
+        Change the command prefix during runtime 
+    """
     client.command_prefix = newprefix
     await ctx.send(f"Set `{newprefix}` as the new command prefix")
     print(f"Set {newprefix} as the new command prefix")
 
 
-# Clear multiple messages in a channel at once, up to 10 at a time.
 @client.command()
 @commands.has_permissions(administrator=True)
 async def clear(ctx, count=3):
+    """
+        Clear multiple messages in a channel at once, up to 10 at a time.
+    """
     if count > 10:
         count = 10
     await ctx.channel.purge(limit=count)
 
 
-# Set role channel.
 @client.command()
 @commands.has_permissions(administrator=True)
 async def setrole(ctx):
+    """
+        Set role channel
+    """
     global ROLE_CHANNEL_ID
     ROLE_CHANNEL_ID = ctx.channel.id
     await ctx.send(f"Set <#{ROLE_CHANNEL_ID}> as default role channel.")
     print(f'Set {ROLE_CHANNEL_ID} as default role channel')
 
 
-# Set role log channel.
 @client.command()
 @commands.has_permissions(administrator=True)
 async def setrolelog(ctx):
+    """
+        Set role log channel
+    """
     global ROLELOG_CHANNEL_ID
     ROLELOG_CHANNEL_ID = ctx.channel.id
     await ctx.send(f"Set <#{ROLELOG_CHANNEL_ID}> as default role log channel.")
     print(f'Set {ROLELOG_CHANNEL_ID} as default role log channel')
 
 
-# Give user a role.
+
 @client.command()
 async def give(ctx, *role_inputs):
+    """
+        Give user a given role.
+    """
     global ROLELOG_CHANNEL_ID
     logchannel = client.get_channel(ROLELOG_CHANNEL_ID)
     user = ctx.message.author
@@ -112,9 +139,11 @@ async def give(ctx, *role_inputs):
             await ctx.message.add_reaction("👍")
 
 
-# Take away user's role.
 @client.command()
 async def remove(ctx, *role_inputs):
+    """
+        Take away user's role.
+    """
     global ROLELOG_CHANNEL_ID
     logchannel = client.get_channel(ROLELOG_CHANNEL_ID)
     user = ctx.message.author
@@ -135,36 +164,5 @@ async def remove(ctx, *role_inputs):
         if success:
             await ctx.message.add_reaction("👍")
 
-# Give all the users in a CSV file a specific role
-@client.command()
-@commands.has_permissions(administrator=True)
-async def bulkgive (ctx, role_input):
-    logchannel = client.get_channel(ROLELOG_CHANNEL_ID)
-    
-    members_given = 0
-    path = os.getcwd() + '/CSVFile.csv'
 
-    # Get attached CSV file
-    attachment_file = ctx.message.attachments[0]
-    role = get(ctx.guild.roles, name=role_input)
-
-    # Save file locally
-    await attachment_file.save(path)
-    
-    with open('CSVFile.csv', 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            try:
-                user = ctx.guild.get_member_named(row[0])
-                await user.add_roles(role)
-                members_given += 1
-            except:
-                await logchannel.send(f'❌ Failed to give {role_input} to {user}')
-    
-    await logchannel.send(f"Gave {members_given} members the role `{role_input}`")
-    
-    os.remove(path)
-            
-            
-if __name__ == '__main__':
-    client.run(os.environ['DISCORD_BOT_TOKEN'])
+client.run(os.environ['DISCORD_BOT_TOKEN'])
